5cd190bbac35f66178dc496d89b7d190feb79908
Mike Usidamen <m.usidamen@gmail.com>
2026-08-04 00:15:22 +0100

    fixes

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
     heading: 'Simplified Payment That Works',
     subheading: 'Secure, seamless transactions for everyone, everywhere, without limits.',
-    centerImageSrc: '/images/herophone.webp',
+    centerImageSrc: '/images/herophone.png',
 }
